926a7835e1b98116bb7cb8f19819144f79b7a2bd
Jay Deverett <jayjdeverett@gmail.com>
2017-08-22 16:05:41 -0400

    display candidates and votes

--- election.js
+++ election.js
@@ -1,5 +1,27 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  var candidates = document.getElementById('candidates');
 
-});
+  $.ajax( {
+  url: 'https://bb-election-api.herokuapp.com/',
+  method: 'GET',
+  dataType: 'json'
+
+  } ).done( function( data ) {
+      console.log("request succeeded");
+      console.log(data);
+
+      for (var i = 0; i < data.candidates.length; i++) {
+
+      var name = data.candidates[i].name
+      var votes = data.candidates[i].votes
+
+      var myElement = document.createElement( 'div');
+      myElement.innerHTML = name + ': ' + votes;
+      candidates.append( myElement );
+
+        }
+    })
+
+    })
+  // Imagination!
